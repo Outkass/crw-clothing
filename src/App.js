a1664544ee94f9932ab96c4d1ea67a1eaea2f08c
Omar <omarpeart25@outlook.com>
2022-01-12 17:31:12 -0500

    Added Sign in components and buttons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SigInAndSignUpPage from './pages/sign-in-and-signup/sign-in-and-signup.component';
 
 function App() {
   return <div> 
@@ -11,7 +12,7 @@ function App() {
    <Switch>
      <Route exact path='/' component={HomePage} />
      <Route path='/shop' component={ShopPage} />
-     
+     <Route path='/signin' component={SigInAndSignUpPage} />
    </Switch>
   </div>
    
---
+++ src/pages/sign-in-and-signup/sign-in-and-signup.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './sign-in-and-signup.styles.scss';
+import SignIn from "../../components/sign-in/sign-in.component";
+
+
+const SigInAndSignUpPage=()=>(
+<div className="sign-in-and-sigup">
+<SignIn/>
+</div>
+
+)
+export default SigInAndSignUpPage;
